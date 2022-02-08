433378699d1235d80f7e00f5c39274ca003db9bd
Benjamin Craddock <benjamincraddock@benjamins-mbp.lan>
2022-02-07 19:46:25 -0500

    pass get endpoints

--- api/server.js
+++ api/server.js
@@ -1,3 +1,43 @@
 // BUILD YOUR SERVER HERE
+const express = require('express')
+const userModel = require('./users/model')
 
-module.exports = {}; // EXPORT YOUR SERVER instead of {}
+const server = express()
+
+server.use(express.json())
+
+// GET ALL USERS endpoint
+server.get('/api/users', (req, res) => {
+  userModel.find()
+    .then(users => {
+      res.json(users)
+    })
+    .catch(() => {
+      res.status(500).json({
+        message: 'The users information could not be retrieved'
+      })
+    })
+})
+
+// GET USER endpoint
+server.get('/api/users/:id', (req, res) => {
+  let { id } = req.params
+  userModel.findById(id)
+    .then(user => {
+      if(!user) {
+        res.status(404).json({
+          message: 'The user with the specified ID does not exist'
+        })
+      } else {
+        res.json(user)
+      }
+    })
+    .catch(() => {
+      res.status(500).json({
+        message: 'The user information could not be retrieved'
+      })
+    })
+})
+
+
+module.exports = server
